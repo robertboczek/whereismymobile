901524963bf67a6a06e14deb171ddb35634de64e
Robert Boczek <robert.boczek@gmail.com>
2013-12-16 01:10:45 -0800

    Device tester controller

--- src/main/java/com/kucowka/whereismymobile/controllers/DeviceController.java
+++ src/main/java/com/kucowka/whereismymobile/controllers/DeviceController.java
@@ -1,6 +1,10 @@
 package com.kucowka.whereismymobile.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,16 +12,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DeviceController {
 
-	@RequestMapping(value="deviceTester")
-	public @ResponseBody String detectDevice(Device device) {
+	@Autowired
+	private DeviceResolver deviceResolver;
+	
+	@RequestMapping(value = "deviceTester")
+	public @ResponseBody String detectDevice(HttpServletRequest request) {
+		
+		Device device = deviceResolver.resolveDevice(request);
+		if (device == null) {
+			return "null";
+		}
 		String deviceType = "unknown";
-        if (device.isNormal()) {
-            deviceType = "normal";
-        } else if (device.isMobile()) {
-            deviceType = "mobile";
-        } else if (device.isTablet()) {
-            deviceType = "tablet";
-        }
-        return "Hello " + deviceType + " browser!";
+		if (device.isNormal()) {
+			deviceType = "normal";
+		} else if (device.isMobile()) {
+			deviceType = "mobile";
+		} else if (device.isTablet()) {
+			deviceType = "tablet";
+		}
+		return "Hello " + deviceType + " browser!";
 	}
 }
